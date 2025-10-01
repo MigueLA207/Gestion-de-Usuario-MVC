c76364957171a351fc76c28f06a6b90d6dfa70e4
Miguelito <mariasmarin981@gmail.com>
2025-10-01 16:00:18 -0500

    commit

--- Migrations/20251001203851_ChangeDateOnly.Designer.cs
+++ Migrations/20251001203851_ChangeDateOnly.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TestMVC.Data;
@@ -11,9 +12,11 @@ using TestMVC.Data;
 namespace TestMVC.Migrations
 {
     [DbContext(typeof(PgDbContext))]
-    partial class PgDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251001203851_ChangeDateOnly")]
+    partial class ChangeDateOnly
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
